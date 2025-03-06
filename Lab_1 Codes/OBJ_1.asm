da56adc08b743eed321af7cb75cb830321d5d81a
Sk Shakeel Akhtar <skkafilakhtar786@gmail.com>
2025-03-06 15:40:35 +0530

    Obj 1 Completed

--- Lab_1 Codes/OBJ_1.asm
+++ Lab_1 Codes/OBJ_1.asm
@@ -1,21 +1,18 @@
-;swapping of nibble 
-mov si, 1000h 
-mov al,[si] ;1st data stores in al 
-mov cl,al 
-inc si 
-mov bl,[si] ;2nd data stores in bl 
-mov dl,al 
-shr al,04 
-shl dl,04 
-or al,dl  
-inc si 
-mov [si],al 
-;Computation of Y 
-mov al,cl 
-and al,bl 
-mov dl,al 
-xor cl,bl 
-or al,cl 
-inc si 
-mov [si],al 
-hlt 
+MOV AX,5225H 
+MOV CX,AX 
+ADD AX,4324H 
+MOV [5000H],AX 
+MOV AX,CX 
+SUB AX,4324H 
+MOV [5002H],AX 
+MOV AX,CX 
+MOV BX,4324H 
+MUL BX 
+MOV [5004H],AX 
+MOV [5006H],DX   
+MOV DX, 0000H 
+MOV AX,CX 
+DIV BX 
+MOV [5008H],AX 
+MOV [500AH],DX 
+HLT
